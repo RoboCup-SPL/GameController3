7d56d2f60c4daa4c532d7095d7f63c13af4addac
Arne Hasselbring <arha@uni-bremen.de>
2023-04-14 09:04:52 +0200

    Use SO_REUSEADDR on Linux and SO_REUSEPORT on macOS (#15)
    ... in team_message_receiver. At least on macOS it is the right thing
    (as it would on other BSDs). I'm not sure if it is harmful to set
    SO_REUSEADDR as well on macOS, and it probably doesn't hurt to use
    SO_REUSEPORT on Linux, but it seems that SO_REUSEADDR on Linux is
    already what SO_REUSEPORT is on BSD, just without TCP / load balancing
    stuff that we don't want anyway.

--- game_controller_net/src/team_message_receiver.rs
+++ game_controller_net/src/team_message_receiver.rs
@@ -1,10 +1,11 @@
 use std::{
     cmp::min,
-    net::{IpAddr, Ipv4Addr, Ipv6Addr},
+    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr},
 };
 
 use anyhow::Result;
 use bytes::Bytes;
+use socket2::{Protocol, SockAddr, Socket, Type};
 use tokio::{net::UdpSocket, sync::mpsc};
 
 use game_controller_msgs::{TEAM_MESSAGE_MAX_SIZE, TEAM_MESSAGE_PORT_BASE};
@@ -25,14 +26,27 @@ pub async fn run_team_message_receiver(
     // Since we want to catch team messages that are too long, we expect one more byte than the
     // maximum message length.
     let mut buffer = vec![0u8; TEAM_MESSAGE_MAX_SIZE + 1];
-    let socket = UdpSocket::bind((
-        match address {
-            IpAddr::V4(_) => IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-            IpAddr::V6(_) => IpAddr::V6(Ipv6Addr::UNSPECIFIED),
-        },
-        TEAM_MESSAGE_PORT_BASE + (team as u16),
-    ))
-    .await?;
+    let socket = {
+        // This might be stuff that should not be done in an async function.
+        let socket_address: SockAddr = SocketAddr::new(
+            match address {
+                IpAddr::V4(_) => IpAddr::V4(Ipv4Addr::UNSPECIFIED),
+                IpAddr::V6(_) => IpAddr::V6(Ipv6Addr::UNSPECIFIED),
+            },
+            TEAM_MESSAGE_PORT_BASE + (team as u16),
+        )
+        .into();
+        let socket = Socket::new(socket_address.domain(), Type::DGRAM, Some(Protocol::UDP))?;
+        #[cfg(target_os = "macos")]
+        socket.set_reuse_port(true)?;
+        #[cfg(target_os = "linux")]
+        socket.set_reuse_address(true)?;
+        // Extend this for other operating systems when it's clear what the right thing is on that
+        // system.
+        socket.bind(&socket_address)?;
+        socket.set_nonblocking(true)?;
+        UdpSocket::from_std(socket.into())?
+    };
     if multicast {
         if let IpAddr::V4(address_v4) = address {
             let _ = socket.join_multicast_v4(Ipv4Addr::new(239, 0, 0, 1), address_v4);
