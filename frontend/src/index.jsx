c33bf70603aa0598aefe179250513f4b5e349d0a
Arne Hasselbring <arha@uni-bremen.de>
2023-04-24 17:55:28 +0200

    Suppress context menu to avoid accidental reloads

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./style.css";
 import Index from "./components/Index";
 
+document.addEventListener("contextmenu", (event) => event.preventDefault());
 const container = document.createElement("div");
 document.body.appendChild(container);
 const root = createRoot(container);
