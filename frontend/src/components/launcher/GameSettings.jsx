a3271d2a3ce1e2c95640a1ff480167adabac4cf8
Arne Hasselbring <arha@uni-bremen.de>
2023-04-24 19:29:04 +0200

    Improve launcher descriptions and mirror teams if set (#10)

--- frontend/src/components/launcher/GameSettings.jsx
+++ frontend/src/components/launcher/GameSettings.jsx
@@ -12,12 +12,16 @@ const GameSettings = ({ teams, game, setGame }) => {
           onChange={(e) => setGame({ ...game, long: e.target.checked })}
         />
       </div>
-      <div className="flex flex-row gap-6">
+      <div
+        className={`flex ${
+          game.sideMapping === "homeDefendsRightGoal" ? "flex-row-reverse" : "flex-row"
+        } gap-6`}
+      >
         {["home", "away"].map((side) => {
           return (
             <div className="flex flex-col items-center gap-2" key={side}>
               <div className="flex flex-row items-center gap-2">
-                <label htmlFor={`kick-off-${side}`}>{side}</label>
+                <label htmlFor={`kick-off-${side}`}>Kick-off for {side} team</label>
                 <input
                   type="radio"
                   checked={game.kickOffSide === side}
@@ -43,7 +47,7 @@ const GameSettings = ({ teams, game, setGame }) => {
         })}
       </div>
       <div className="flex flex-row items-center gap-2">
-        <label htmlFor="mirror">Mirror</label>
+        <label htmlFor="mirror">Mirror (home team starts on right side)</label>
         <input
           type="checkbox"
           checked={game.sideMapping === "homeDefendsRightGoal"}
