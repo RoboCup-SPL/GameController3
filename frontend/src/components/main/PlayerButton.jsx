13fa180a8447c4f0d5a5cb048d95a780a869c9c9
Arne Hasselbring <arha@uni-bremen.de>
2023-04-27 13:02:50 +0200

    Use tabular-nums also for penalty timers

--- frontend/src/components/main/PlayerButton.jsx
+++ frontend/src/components/main/PlayerButton.jsx
@@ -30,9 +30,10 @@ const PlayerButton = ({ color, legal, sign, onClick, player }) => {
         <div className="grow flex flex-col">
           <p>{color.charAt(0).toUpperCase() + color.slice(1)}</p>
           <p
-            className={
+            className={`${player.penaltyTimer.started ? "tabular-nums" : ""} ${
               player.penaltyTimer.started || player.penalty != "noPenalty" ? "" : "invisible"
             }
+            `}
           >
             {player.penaltyTimer.started ? formatMMSS(player.penaltyTimer) : "Penalized"}
           </p>
