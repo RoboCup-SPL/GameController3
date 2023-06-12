8f528c1902d20c541dcb62f315815d819062a1be
Arne Hasselbring <arha@uni-bremen.de>
2023-06-12 17:40:12 +0200

    Make the GameController aware of technical challenges (#22)

--- game_controller_core/src/types.rs
+++ game_controller_core/src/types.rs
@@ -10,6 +10,14 @@ use serde::{Deserialize, Serialize};
 
 use crate::timer::Timer;
 
+/// This enumerates the special GameController modes for technical challenges.
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum ChallengeMode {
+    /// Dynamic Ball Handling Challenge
+    DynamicBallHandling,
+}
+
 /// This struct contains constant parameters of a penalty type.
 #[derive(Clone, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -38,6 +46,8 @@ pub struct SetPlayParams {
 pub struct CompetitionParams {
     /// A "pretty" version of the competition's name.
     pub name: String,
+    /// The mode (if this is a technical challenge).
+    pub challenge_mode: Option<ChallengeMode>,
     /// The number of players per team that can play at the same time.
     pub players_per_team: u8,
     /// The parameters of each penalty type.
