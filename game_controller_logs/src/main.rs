37c592807502d748f4092263752c20d948d71c26
Arne Hasselbring <arha@uni-bremen.de>
2023-06-21 15:17:21 +0200

    Add a main program for log stuff

---
+++ game_controller_logs/src/main.rs
@@ -0,0 +1,59 @@
+//! This crate defines the main program to analyze GameController log files.
+
+use std::{fs::File, path::PathBuf};
+
+use anyhow::{Context, Result};
+use clap::{Parser, Subcommand};
+
+use game_controller_core::log::TimestampedLogEntry;
+
+use game_controller_logs::{data_minimization_challenge, visual_referee_challenge};
+
+/// This struct defines the parser for the command line arguments.
+#[derive(Parser)]
+#[command(about, author, version)]
+struct Args {
+    /// The path of the log file to analyze.
+    #[arg(long, short)]
+    pub path: Option<PathBuf>,
+    /// The kind of thing that should be done with that log file.
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+/// This struct defines the command line subcommands.
+#[derive(Subcommand)]
+enum Commands {
+    /// Extract statistics for the data minimization challenge.
+    DataMinimizationChallenge,
+    /// Extract statistics for the visual referee challenge.
+    VisualRefereeChallenge,
+}
+
+/// This function applies a subcommand to one log file.
+fn process_file(f: File, command: &Commands) -> Result<()> {
+    let entries: Vec<TimestampedLogEntry> =
+        serde_yaml::from_reader(f).context("could not parse log file")?;
+    match command {
+        Commands::DataMinimizationChallenge => {
+            data_minimization_challenge::evaluate(entries)
+                .context("could not evaluate data minimization challenge")?;
+        }
+        Commands::VisualRefereeChallenge => {
+            visual_referee_challenge::evaluate(entries)
+                .context("could not evaluate visual referee challenge")?;
+        }
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    if let Some(path) = args.path {
+        let f = File::open(path).context("could not open log file")?;
+        process_file(f, &args.command)?;
+    }
+
+    Ok(())
+}
