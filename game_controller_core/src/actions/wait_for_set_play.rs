a89dc01e6dee3d10d1d3b1a38cbaadb2dbb7f0dd
Arne Hasselbring <arha@uni-bremen.de>
2025-02-08 11:04:56 +0100

    Clear motion in standby penalties at transition to Set
    I'm not sure if this is complete or if I need to consider other
    transitions as well.

--- game_controller_core/src/actions/wait_for_set_play.rs
+++ game_controller_core/src/actions/wait_for_set_play.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::action::{Action, ActionContext};
 use crate::timer::Timer;
-use crate::types::{SetPlay, State};
+use crate::types::{Penalty, SetPlay, State};
 
 /// This struct defines an action which corresponds to the referee call "Set". It is the second
 /// part of "complex" set plays which have a Ready and Set state.
@@ -11,6 +11,16 @@ pub struct WaitForSetPlay;
 
 impl Action for WaitForSetPlay {
     fn execute(&self, c: &mut ActionContext) {
+        c.game.teams.values_mut().for_each(|team| {
+            team.players
+                .iter_mut()
+                .filter(|player| player.penalty == Penalty::MotionInStandby)
+                .for_each(|player| {
+                    player.penalty = Penalty::NoPenalty;
+                    player.penalty_timer = Timer::Stopped;
+                })
+        });
+
         c.game.secondary_timer = Timer::Stopped;
         c.game.state = State::Set;
     }
